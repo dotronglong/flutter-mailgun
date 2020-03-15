268478db5a587aaad01ffccb3df0dca8aad6553d
Lucas <me@dotronglong.com>
2020-03-15 08:39:46 +0800

    v0.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.0.2
+
+* Upgrade `mail: 0.0.3`
+
 ## 0.0.1
 
 * Initial release
--- lib/src/mailgun_mailer.dart
+++ lib/src/mailgun_mailer.dart
@@ -17,42 +17,53 @@ class MailgunMailer implements Mailer {
       List<String> to = const [],
       List<String> cc = const [],
       List<String> bcc = const [],
-      List attachments = const [],
+      List<dynamic> attachments = const [],
       String subject,
       String html,
-      String text}) async {
+      String text,
+      String template,
+      Map<String, dynamic> options}) async {
     var client = http.Client();
-    var request = http.Request(
-        'POST',
-        Uri(
-            userInfo: 'api:$apiKey',
-            scheme: 'https',
-            host: 'api.mailgun.net',
-            path: '/v3/$domain/messages'));
-    var body = Map<String, String>();
-    if (subject != null) {
-      body['subject'] = subject;
-    }
-    if (html != null) {
-      body['html'] = html;
-    }
-    if (text != null) {
-      body['text'] = text;
-    }
-    if (from != null) {
-      body['from'] = from;
-    }
-    if (to.length > 0) {
-      body['to'] = to.join(", ");
-    }
-    if (cc.length > 0) {
-      body['cc'] = cc.join(", ");
-    }
-    if (bcc.length > 0) {
-      body['bcc'] = bcc.join(", ");
-    }
-    request.bodyFields = body;
     try {
+      var request = http.Request(
+          'POST',
+          Uri(
+              userInfo: 'api:$apiKey',
+              scheme: 'https',
+              host: 'api.mailgun.net',
+              path: '/v3/$domain/messages'));
+      var body = Map<String, String>();
+      if (subject != null) {
+        body['subject'] = subject;
+      }
+      if (html != null) {
+        body['html'] = html;
+      }
+      if (text != null) {
+        body['text'] = text;
+      }
+      if (from != null) {
+        body['from'] = from;
+      }
+      if (to.length > 0) {
+        body['to'] = to.join(", ");
+      }
+      if (cc.length > 0) {
+        body['cc'] = cc.join(", ");
+      }
+      if (bcc.length > 0) {
+        body['bcc'] = bcc.join(", ");
+      }
+      if (template != null) {
+        body['template'] = template;
+      }
+      if (options != null) {
+        if (options.containsKey('template_variables')) {
+          body['h:X-Mailgun-Variables'] =
+              jsonEncode(options['template_variables']);
+        }
+      }
+      request.bodyFields = body;
       var response = await client.send(request);
       var responseBody = await response.stream.bytesToString();
       var jsonBody = jsonDecode(responseBody);
